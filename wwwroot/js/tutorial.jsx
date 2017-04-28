88ab4dd7878f4c442d4c92ca5213e91ea210a8e4
Luis Bosquez <luisdbosquez@gmail.com>
2017-04-28 16:00:33 -0700

    Before experimenting with Jint

---
+++ wwwroot/js/tutorial.jsx
@@ -0,0 +1,16 @@
+alert("loaded");
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
+
