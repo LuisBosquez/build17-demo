b6b8bca27d016bbd9ecada00a444f61361368937
Luis Bosquez <luisdbosquez@gmail.com>
2017-05-09 16:26:24 -0700

    removing test data, adding env variable config

--- Startup.cs
+++ Startup.cs
@@ -19,9 +19,14 @@ namespace build17_demo
     public class Startup
     {
         public static SqlConnectionStringBuilder connectionBuilder = new SqlConnectionStringBuilder();
-            
+        
         public Startup(IHostingEnvironment env)
         {
+            var server = Configuration["DatabaseServer"];
+            var database = Configuration["DatabaseName"];
+            var user = Configuration["DatabaseUser"];
+            var password = Configuration["DatabaseUserPassword"];
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -29,10 +34,10 @@ namespace build17_demo
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
 
-            connectionBuilder.DataSource = "localhost";
-            connectionBuilder.UserID = "sa";
-            connectionBuilder.Password = "Luis9000"; 
-            connectionBuilder.InitialCatalog = "EFSampleDB";
+            connectionBuilder.DataSource = server;
+            connectionBuilder.UserID = user;
+            connectionBuilder.Password = password; 
+            connectionBuilder.InitialCatalog = database;
         }
 
         public IConfigurationRoot Configuration { get; }
